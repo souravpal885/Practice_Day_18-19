7d272c71a4733935eaf78209d656c7cea03bbd7e
souravpal885 <souravpal885@gmail.com>
2023-04-19 15:45:59 +0530

    initial commit.

---
+++ Practice_Day_18-19/src/com/exception/_1_errors.java
@@ -0,0 +1,10 @@
+package com.exception;
+
+public class _1_errors {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 9;
+        System.out.println(a+b);
+    }
+}
+
---
+++ Practice_Day_18-19/src/com/exception/_3_try_catch.java
@@ -0,0 +1,54 @@
+package com.exception;
+import java.util.Scanner;
+
+/*public class _2_try_catch {
+    public static void main(String[] args) {
+        int a = 6000;
+        int b = 0;
+        // Without Try:
+//        int c = a / b;
+//        System.out.println("The result is " + c);
+        // With Try:
+        try {
+            int c = a / b;
+            System.out.println("The result is " + c);
+        }
+        catch(Exception e) {
+            System.out.println("We failed to divide. Reason: ");
+            System.out.println(e);
+        }
+        System.out.println("End of the program");
+    }
+}
+*/
+
+public class _3_try_catch {
+    public static void main(String[] args) {
+        int [] marks = new int[3];
+        marks[0] = 7;
+        marks[1] = 56;
+        marks[2] = 6;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array index");
+        int ind = sc.nextInt();
+
+        System.out.println("Enter the number you want to divide the value with");
+        int number = sc.nextInt();
+        try{
+            System.out.println("The value at array index entered is: " + marks[ind]);
+            System.out.println("The value of array-value/number is: " + marks[ind]/number);
+        }
+        catch (ArithmeticException e){
+            System.out.println("ArithmeticException occured!");
+            System.out.println(e);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("ArrayIndexOutOfBoundsException occured!");
+            System.out.println(e);
+        }
+        catch (Exception e){
+            System.out.println("Some other exception occured!");
+            System.out.println(e);
+        }
+    }
+}
